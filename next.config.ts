d5102cd7e752c0fcce706bd822dbcfdd618267b9
Vasvi Saini <vasvisaini60@gmail.com>
2025-09-08 00:09:14 +0530

    test 1

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
 import type { NextConfig } from "next";
-import { hostname } from "os";
+
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-     hostname: "**",
-      }
-      
-    ]
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [
+        ...(config.plugins ?? []),
+        new PrismaPlugin(),
+      ];
+    }
+    return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
